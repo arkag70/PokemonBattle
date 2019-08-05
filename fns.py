393fcf69a196e8fa3702f3025762909029f2c736
Arkaprabha Ghosh <arkag70@gmail.com>
2019-08-05 17:10:48 +0530

    type part updated

--- fns.py
+++ fns.py
@@ -42,12 +42,24 @@ class Pokemon:
 	def getMoves(self):
 		moves = []
 		if self.type1 == self.type2:
+			print(self.type1,self.type2,"same type")
 			for i in range(4):
-				key, value = list(moveset[self.type1].items())[i]
+				key, value = list(moveset[self.type1].items())[random.randint(0,10)]
 				moves.append(key)
+				
 		else:
+			print(self.type1,self.type2,"different types")
 			for i in range(2):
-				key, value = list(moveset[self.type1].items())[i]
-				key, value = list(moveset[self.type2].items())[i]
+				key, value = list(moveset[self.type1].items())[random.randint(0,10)]
 				moves.append(key)
+			for i in range(2):
+				key, value = list(moveset[self.type2].items())[random.randint(0,10)]
+				moves.append(key)
+		print(moves)
 		return moves
+
+# def hello():
+# 	key, value = list(moveset["dark"].items())[5]
+# 	print(key)
+
+# hello()
